3a17a62b4f22e2dea2dc4d79c20a867369449b73
緑川実生 <mio@midorikawa-MacBook-Air.local>
2021-09-09 01:00:23 +0900

    makefile

--- test3/listdata.c
+++ test3/listdata.c
@@ -45,3 +45,10 @@ void	assign_out2(out2 *out, int instruction, t_list *a, t_list *b)
 	out->pa = a;
 	out->pb = b;
 }
+
+void	assign_out2(out2 *out, int instruction, t_list *a, t_list *b)
+{
+	out->instruction = instruction;
+	out->pa = a;
+	out->pb = b;
+}
